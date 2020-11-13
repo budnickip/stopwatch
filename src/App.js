aca09943b6e6778d06028d9237ed90c892055f41
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2020-11-13 12:45:05 +0100

    11 na 29 bez 8

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import './App.scss';
 function App() {
   const [breakLength, setBreakLength] = useState(5)
   const [sessionLength, setSessionLength] = useState(25)
+  const [timer, setTimer] = useState(sessionLength*60)
+
+  const showTime = () =>{
+    let minutes = Math.floor(timer/60);
+    let seconds = timer - minutes * 60;
+    minutes = minutes < 10 ? '0' + minutes : minutes
+    seconds = seconds < 10 ? '0' + seconds : seconds
+    return minutes + ':' + seconds;
+  }
   return (
     <div className="App">
       <div id="break-label">
@@ -18,10 +27,17 @@ function App() {
         <p id="session-length">{sessionLength}</p>
         <button id="session-increment">+</button>
       </div>
-      <div id="timer-label">
-        <p>Session</p>
-        
+      <div className="timer">
+        <div id="timer-label">
+          <p>Session</p>
+        </div>
+        <div id="time-left">
+            {showTime()}
+        </div>
+        <button id="start_stop">Start_Stop</button>
+        <button id="reset">reset</button>
       </div>
+
     </div>
   );
 }
