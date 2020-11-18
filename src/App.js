7c0024e0a6108ab06585b08e4760ff8021f8601b
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2020-11-18 13:49:47 +0100

    18/29 dodany useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.scss';
 
 function App() {
   const [breakLength, setBreakLength] = useState(5)
   const [sessionLength, setSessionLength] = useState(25)
   const [timer, setTimer] = useState(sessionLength*60)
+  const [minutes, setMinutes] = useState(25)
+  const [seconds, setSeconds] = useState(0)
+
+  const updateTime = () =>{
+    setMinutes(Math.floor(timer/60))
+    setSeconds(timer - minutes *60);
+    if(minutes < 10){
+      setMinutes('0'+minutes)
+    }
+    if(seconds < 10){
+      setSeconds('0'+seconds)
+    }
+  }
 
   const showTime = () =>{
     let minutes = Math.floor(timer/60);
@@ -12,13 +25,14 @@ function App() {
     minutes = minutes < 10 ? '0' + minutes : minutes
     seconds = seconds < 10 ? '0' + seconds : seconds
     return minutes + ':' + seconds;
-  }
+  } 
 
   const reset = () =>{
     setBreakLength(5)
     setSessionLength(25)
     setTimer(sessionLength*60)
   }
+
 
   const breakDecrement = () =>{
     if(breakLength > 1){
@@ -32,19 +46,28 @@ function App() {
     }
   }
 
+
   const sessionDecrement = () => {
     if(sessionLength > 1){
       setSessionLength(sessionLength-1)
     }
-    setTimer(sessionLength*60)
+    //setTimer(sessionLength*60)
   }
 
   const sessionIncrement = () =>{
     if(sessionLength < 60){
       setSessionLength(sessionLength+1)
     }
-    setTimer(sessionLength*60)
+    //setTimer(sessionLength*60) z useState
   }
+
+    //useState jest asynchroniczne, dlatego jest konieczność użycia useEffect, w innym przypadku
+    //wynik byłby aktualizowany dopiero w kolejnym ruchu!
+    // po dodaniu dependency array(drugi argument useEffect), useEffect będzie wywoływane tylko
+    // w momencie w którym sessionLength zostanie zaktualizowany!
+  useEffect(()=>{
+      setTimer(sessionLength*60)
+  }, [sessionLength])
   return (
     <div className="App">
       <div id="break-label">
