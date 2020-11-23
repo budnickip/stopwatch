8e507e01cda2c058948a9f6285c1a45f74a6499b
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2020-11-23 14:54:53 +0100

    poprawne wyświetlanie stopera

--- src/containers/StopWatch/index.js
+++ src/containers/StopWatch/index.js
@@ -3,16 +3,21 @@ import { useEffect, useState } from "react"
 function StopWatch() {
     const [startTime, setStartTime] = useState(0)
     const [running, setRunning] = useState(false)
+    const [paused, setPaused] = useState(false)
     const [endTime, setEndTime] = useState(0)
-    const [stoper, setStoper] = useState("00:00:00")
+    const [stoper, setStoper] = useState(0)
+  //  const [displayer, setDisplayer] = useState("00:00:00");
 
     const measureTime = () =>{
-        setStartTime(performance.now())
+        if(!running){
+            setStartTime(performance.now())
+        }
         setRunning(true)
+        setPaused(false)
     }
     const stopTime = () =>{
         setEndTime(performance.now())
-        setRunning(false)
+        setPaused(true)
     }
     const resetTime = () =>{
         setRunning(false)
@@ -21,7 +26,7 @@ function StopWatch() {
 
     useEffect(()=>{
         const intervalStoper = setInterval(() => {
-             if(running){
+             if(running && !paused){
                 setStoper(Math.round(performance.now()-startTime))
              }else{
              //  handleBreak()
@@ -30,10 +35,35 @@ function StopWatch() {
            }, 10);
            return () => clearInterval(intervalStoper);
          })
+
+    const displayer = () =>{
+
+        var milliseconds, seconds, minutes, hours
+        if(running){
+             milliseconds = parseInt((stoper%1000))
+             seconds = parseInt((stoper/1000)%60)
+             minutes = parseInt((stoper/(1000*60))%60)
+             hours = parseInt((stoper/(1000*60*60))%24);
+        
+        hours = (hours < 10) ? "0" + hours : hours;
+        minutes = (minutes < 10) ? "0" + minutes : minutes;
+        seconds = (seconds < 10) ? "0" + seconds : seconds;
+        
+        
+        }else{
+            milliseconds = "00"
+            seconds = "00"
+            minutes = "00"
+            hours = "00"
+        }
+
+        return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+
+    }
     return(
         <div className="stopwatch">
             <h1 className="stopwatch__header">Stoper</h1>
-            <div className="time">{stoper}</div>
+            <div className="time">{displayer()}</div>
             <button onClick={measureTime}>Start</button>
             <button onClick={stopTime}>Stop</button>
             <button onClick={resetTime}>Reset</button>
