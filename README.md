0c2e414078b075e6b184130c6274117f6f689f1e
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2020-11-23 15:30:05 +0100

    poprawione style

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 ### Wersja rozszerzona:
 
 Live: [https://budnickip.github.io/stopwatch/](https://budnickip.github.io/stopwatch/)
+Czas w Timerze jest mierzony jedynie z wykorzystaniem setInterval, natomiast w Stoperze, do mierzenia czasu została wykorzystana
+funkcja performance.now()(mierzenie czasu w stoperze jest bardziej dokładne)\
 
 ### Do zaliczenia na freeCodeCamp, wersja podstawowa:
 
--- src/containers/StopWatch/index.js
+++ src/containers/StopWatch/index.js
@@ -3,24 +3,25 @@ import { useEffect, useState } from "react"
 function StopWatch() {
     const [startTime, setStartTime] = useState(0)
     const [running, setRunning] = useState(false)
-    const [paused, setPaused] = useState(false)
+    const [paused, setPaused] = useState(true)
     const [endTime, setEndTime] = useState(0)
     const [stoper, setStoper] = useState(0)
+    const [name, setName] = useState(paused ? " far fa-play-circle" : "far fa-pause-circle")
+    const [hoveredStart, setHoveredStart] = useState(false)
   //  const [displayer, setDisplayer] = useState("00:00:00");
 
-    const measureTime = () =>{
+
+    const handleStart = () =>{
         if(!running){
             setStartTime(performance.now())
         }
         setRunning(true)
-        setPaused(false)
+        setPaused(!paused)
     }
-    const stopTime = () =>{
-        setEndTime(performance.now())
-        setPaused(true)
-    }
+
     const resetTime = () =>{
         setRunning(false)
+        setPaused(true)
         setStoper("00:00:00")
     }
 
@@ -40,7 +41,7 @@ function StopWatch() {
 
         var milliseconds, seconds, minutes, hours
         if(running){
-             milliseconds = parseInt((stoper%1000))
+             milliseconds = parseInt(((stoper/10)%100))
              seconds = parseInt((stoper/1000)%60)
              minutes = parseInt((stoper/(1000*60))%60)
              hours = parseInt((stoper/(1000*60*60))%24);
@@ -48,7 +49,7 @@ function StopWatch() {
         hours = (hours < 10) ? "0" + hours : hours;
         minutes = (minutes < 10) ? "0" + minutes : minutes;
         seconds = (seconds < 10) ? "0" + seconds : seconds;
-        
+        milliseconds = (milliseconds < 10) ? "0" + milliseconds : milliseconds;
         
         }else{
             milliseconds = "00"
@@ -57,16 +58,29 @@ function StopWatch() {
             hours = "00"
         }
 
-        return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+        return hours + ":" + minutes + ":" + seconds + ":" + milliseconds;
 
     }
+
+    const enterName = () =>{
+        setHoveredStart(true)
+      }
+    
+      const leaveName = () =>{
+        setHoveredStart(false)
+      }
+    
+      useEffect(()=>{
+        hoveredStart ? setName(paused ? "fas fa-play-circle" : "fas fa-pause-circle") : setName(paused ? " far fa-play-circle" : "far fa-pause-circle")
+      },[hoveredStart,paused])
     return(
         <div className="stopwatch">
-            <h1 className="stopwatch__header">Stoper</h1>
+            <div className="stopwatch__header">
+                 <h1>Stoper</h1>
+            </div>
             <div className="time">{displayer()}</div>
-            <button onClick={measureTime}>Start</button>
-            <button onClick={stopTime}>Stop</button>
-            <button onClick={resetTime}>Reset</button>
+            <i id="start_stop" onClick={handleStart} onMouseEnter={enterName} onMouseLeave={leaveName} className={`icon ${name}`} />
+            <i id="reset" onClick={resetTime} class="fas icon reset-icon fa-sync"></i>
         </div>
     )
 }
